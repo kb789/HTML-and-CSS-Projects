7389a2eeb8e14c70bc13cc78abc8f59cd726c575
kb789 <bucher.kristina@gmail.com>
2023-04-25 16:43:31 -0400

    BASIC HTML AND JAVASCRIPT 1 SUBMISSION ASSIGNMENT

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello world!")
+
+function MyFirstFunction() {
+	      var String = "kiss me, I'm irish";
+		  var result = String.fontcolor("green");
+		
+		  document.getElementById("irish").innerHTML=result;
+	  }
